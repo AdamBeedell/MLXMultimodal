ff82b7140e55661e9bf528e3b8cf7b6f52977eae
Dan G <freemvmt@posteo.net>
2025-06-30 18:29:20 +0100

    freemvmt: start investigating models

--- freemvmt/main.py
+++
@@ -1,7 +0,0 @@
-from transformers import CLIPModel, ViTModel
-
-c = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
-v = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
-
-print("CLIP:", c)
-print("ViTL", v)
